9669f29728355758f97765f4621065c000756df3
edonkoh <64780815+edonkoh@users.noreply.github.com>
2022-12-05 09:55:42 -0400

    Add data processing file
    + Loads messages and categories data.
    + cleans categories
    + saves into a database

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,50 @@
-import sys
+import pandas as pd
+from sqlalchemy import create_engine 
+import sys 
+
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df= messages.merge(categories, on= 'id', how= 'inner')
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat= ';', n= -1, expand= True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
 
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    # category_colnames = row.str.split(pat='-', n=  1, expand= True)[0] # This implementation uses pandas function to exttract the column names. 
+    category_colnames = row.apply(lambda x: x[0:len(x)-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df= df.drop('categories', axis= 1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis =1)
+    
+    df = df.drop_duplicates()
+    
+    return(df)
 
 def save_data(df, database_filename):
-    pass  
-
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('messages', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
@@ -21,7 +54,8 @@ def main():
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
-
+        
+        
         print('Cleaning data...')
         df = clean_data(df)
         
